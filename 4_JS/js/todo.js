8613d4ec51a7d26dde3d82861fd71e7cb516af97
DeepMakhasana <deepmakhasana1013@gmail.com>
2024-08-12 16:22:40 +0530

    Task manager structure created with style

---
+++ 4_JS/js/todo.js
@@ -0,0 +1,6 @@
+// document.write(
+//   '<div class="input-group mb-3"><div class="input-group-text"><input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input"></div><input type="text" class="form-control" value="Learn Javascript from basic to advance" disabled><span class="input-group-text" style="cursor: pointer;">Edit</span><span class="input-group-text" style="cursor: pointer;">Delete</span></div>'
+// );
+
+// form references
+const taskInput = document.getElementById("taskInput");
